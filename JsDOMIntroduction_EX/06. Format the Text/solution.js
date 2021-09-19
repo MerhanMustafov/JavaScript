186b2bec95ff93d4ef47ec9bb2c7a5e3ff948e6d
unknown <m.o.mustafov@gmail.com>
2021-09-19 13:58:21 +0300

    add solution to 6th ex from domIntoEx

--- JsDOMIntroduction_EX/06. Format the Text/solution.js
+++ JsDOMIntroduction_EX/06. Format the Text/solution.js
@@ -1,23 +1,22 @@
 function solve() {
   let roughText = document.querySelector('#input').value;
-  console.log(roughText);
-  console.log(roughText.value);
 
   let arrayOfSentences = roughText.split('.')
-  console.log(arrayOfSentences);
-
   let divTagOutput = document.querySelector('#output')
   let paragraphs = []
   
   for (let i = 0; i < arrayOfSentences.length; i++){
+    let currentSentence = arrayOfSentences[i]
+    if (currentSentence.length > 0){
+      paragraphs.push(arrayOfSentences[i])
+    }
     
-    paragraphs.push(arrayOfSentences[i])
     if (paragraphs.length == 3){
       paragraphs[paragraphs.length - 1] += '.'
       let result = paragraphs.join('. ')
       
       divTagOutput.innerHTML += `<p>${result}</p>`
-      paragraphs.length = 0
+      paragraphs.length = []
     }  
       
   }
@@ -30,16 +29,13 @@ function solve() {
 
 
 
-let str = `JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a
-language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. Alongside
-HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables
-interactive web pages and thus is an essential part of web applications. The vast majority of websites use it,
-and all major web browsers have a dedicated JavaScript engine to execute it. As a multi-paradigm language,
-JavaScript supports event-driven, functional, and imperative (including object-oriented and prototype-based)
-programming styles. It has an API for working with text, arrays, dates, regular expressions, and basic
-manipulation of the DOM, but the language itself does not include any I/O, such as networking, storage, or
-graphics facilities, relying for these upon the host environment in which it is embedded.`
+// let str = `JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a
+// language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. Alongside
+// HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables
+// interactive web pages and thus is an essential part of web applications. The vast majority of websites use it,
+// and all major web browsers have a dedicated JavaScript engine to execute it. As a multi-paradigm language,
+// JavaScript supports event-driven, functional, and imperative (including object-oriented and prototype-based)
+// programming styles. It has an API for working with text, arrays, dates, regular expressions, and basic
+// manipulation of the DOM, but the language itself does not include any I/O, such as networking, storage, or
+// graphics facilities, relying for these upon the host environment in which it is embedded.`
 
-// let array = [1, 2, 3]
-// array.length = 0
-// console.log(array)
