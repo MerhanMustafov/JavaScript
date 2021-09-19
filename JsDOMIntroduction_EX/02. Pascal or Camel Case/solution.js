640ccb03b808446ffc9b70842d31db971f10ce7d
unknown <m.o.mustafov@gmail.com>
2021-09-19 13:09:19 +0300

    add solution to domIntoEx task 2

--- JsDOMIntroduction_EX/02. Pascal or Camel Case/solution.js
+++ JsDOMIntroduction_EX/02. Pascal or Camel Case/solution.js
@@ -1,51 +1,39 @@
-function camelCase(input){
-  let result = ''
-  for (let i = 0; i < input.length; i++){
-    if (input[i] == ' '){
-      continue;
-    }else if (input[i-1]==' '){
-      let upper = input[i].toUpperCase()
-      result += upper
-      continue;
-    }
-    result += input[i]
-  }
-  return result
-}
-function pascalCase(input){
-  let result = ''
-  for (let i = 0; i < input.length; i++){
-    if (i == 0){
-      let upper = input[i].toUpperCase()
-      result += upper
-      continue;
-    }else if (input[i-1]==' '){
-      let upper = input[i].toUpperCase()
-      result += upper
-      continue;
-    }else if (input[i] == ' '){
-      continue;
-    }
-    result += input[i]
-  }
-  return result
-}
 function solve() {
-  let input = document.getElementById('text').value;
+  let input = document.getElementById('text').value.toLowerCase().split(' ');
   let currentCase = document.getElementById('naming-convention').value;
 
-  
-  // currentCase = currentCase.replace(/^"|"$/g, "");
-  
-  input = input.toLowerCase()
   let result;
   if (currentCase == "Camel Case"){
-    result = camelCase(input)
+    // for (let i = 0; i < input.length; i++){
+    //   if (i !== 0){
+    //     input[i] = input[i].charAt(0).toLocaleUpperCase() + input[i].slice(1)
+    //   }
+    //   result = input.join('')
+    // }
+    
+    // WITH MAP
+    result = input.map((el, index) => index !== 0 ? el.charAt(0).toUpperCase() + el.slice(1) : el).join('')
+    
   }else if (currentCase == "Pascal Case"){
-    result = pascalCase(input)
-  }else{
-    result = 'Error!'
-  }
-  document.getElementById('result').textContent = result
+    // for (let i = 0; i < input.length; i++){
+    //   input[i] = input[i].charAt(0).toUpperCase() + input[i].slice(1)
+    // }
+    // result = input.join('')
 
+    // WITH MAP
+    result = input.map((el, index) => el.charAt(0).toUpperCase() + el.slice(1)).join('')
+
+  }else{
+    result = "Error!"
+  }
+  document.getElementById('result').innerHTML = result
 }
+
+
+// let arrayOfString = ['aaa', 'bbb', 'ccc']
+
+
+// arrayOfString = arrayOfString.map((el, index) => index !== 0 ? el.charAt(0).toUpperCase() + el.slice(1) : el)
+// // arrayOfString.map(el => console.log(el.charAt(0).toUpperCase() + el.slice(1)))
+
+// console.log(arrayOfString)
