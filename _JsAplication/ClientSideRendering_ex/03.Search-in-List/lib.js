f3d70e9613734df7f4fdfc9b91897c60738ca0d7
unknown <m.o.mustafov@gmail.com>
2021-11-20 18:08:47 +0200

    add

---
+++ _JsAplication/ClientSideRendering_ex/03.Search-in-List/lib.js
@@ -0,0 +1,4 @@
+import { html, render} from './node_modules/lit-html/lit-html.js'
+import {towns} from './towns.js'
+
+export {html, render, towns}
--- _JsAplication/ClientSideRendering_ex/03.Search-in-List/search.js
+++ _JsAplication/ClientSideRendering_ex/03.Search-in-List/search.js
@@ -1,3 +1,26 @@
-function search() {
-   // TODO
+import {html, render , towns} from './lib.js'
+
+// TEMPLATE
+const townsTemplate = (town) => html`<li>${town}</li>`
+
+
+// Load on reloadPage
+const divTowns = document.getElementById('towns');
+const ul = document.createElement('ul'); divTowns.appendChild(ul);
+render(towns.map(town => townsTemplate(town)), ul)
+
+
+// earch for matches on click
+document.querySelector('button').addEventListener('click', search)
+function search(e) {
+   const inputBox = document.getElementById('searchText');
+   if (inputBox.value){
+      [...divTowns.firstElementChild.children].forEach(town => {
+         // console.log('BEFORE IF:', town.textContent.toLowerCase().indexOf(inputBox.value.toLowerCase()))
+         if (town.textContent.toLowerCase().indexOf(inputBox.value.toLowerCase()) != -1){
+            town.style['font-weight'] = 'bold'
+            // console.log('AFTER IF:',town.textContent.toLowerCase().indexOf(inputBox.value.toLowerCase()))
+         }
+      })
+   }
 }
